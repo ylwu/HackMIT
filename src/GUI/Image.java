ac1f79d01b8740a5aa6053c224c0f2a56b1e6b1a
Robert <robertluo91@gmail.com>
2013-10-06 00:53:31 -0400

    robert

---
+++ src/GUI/Image.java
@@ -0,0 +1,30 @@
+package GUI;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class Image extends JPanel{
+
+    private BufferedImage image;
+
+    public Image() {
+       try {                
+          image = ImageIO.read(new File("/pictures/image1.JPEG"));
+       } catch (IOException ex) {
+            // handle exception...
+       }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, null); // see javadoc for more info on the parameters            
+    }
+
+}
